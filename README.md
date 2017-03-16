11512e1debfd47b2e792cbe9870125b0e13e368c
Lazzlo2096 <lazzlo2096@yandex.ru>
2017-03-16 15:36:04 +0300

    Рефакторинг кода; Добавил README файл

---
+++ README.md
@@ -0,0 +1,70 @@
+# MaPong
+> Простая игра
+
+<!-- [![NPM Version][npm-image]][npm-url] -->
+<!-- [![Build Status][travis-image]][travis-url] -->
+<!-- [![Downloads Stats][npm-downloads]][npm-url] -->
+
+Ping-pong игра на Allegro 5 с использованием [ALX](https://github.com/axilmar/ALX) (C++11 Allegro 5 wrapper library)
+
+![](maPong_screenshot.png)
+
+## Installation
+
+Linux:
+
+```sh
+sudo apt install liballegro5-dev
+git clone https://github.com/Lazzlo2096/maPong.git
+cd maPong
+git clone https://github.com/axilmar/ALX.git
+make
+```
+
+<!-- Windows:
+
+```sh
+edit autoexec.bat
+``` -->
+
+<!-- ## Usage example
+
+A few motivating and useful examples of how your product can be used. Spice this up with code blocks and potentially more screenshots.
+
+## Development setup
+
+Describe how to install all development dependencies and how to run an automated test-suite of some kind. Potentially do this for multiple platforms.
+
+```sh
+make install
+npm test
+``` -->
+
+## Release History
+
+<!-- * 0.2.1
+    * CHANGE: Update docs (module code remains unchanged)
+* 0.2.0
+    * CHANGE: Remove `setDefaultXYZ()`
+    * ADD: Add `init()`
+* 0.1.1
+    * FIX: Crash when calling `baz()` (Thanks @GenerousContributorName!)
+* 0.1.0
+    * The first proper release
+    * CHANGE: Rename `foo()` to `bar()` -->
+* 0.0.1
+    * Work in progress
+
+## Meta
+
+<!-- Your Name – [@YourTwitter](https://twitter.com/dbader_org) – YourEmail@example.com -->
+
+Distributed under the LGPL-3.0 license. See ``LICENSE`` for more information.
+
+[https://github.com/Lazzlo2096](https://github.com/Lazzlo2096)
+
+<!-- [npm-image]: https://img.shields.io/npm/v/datadog-metrics.svg?style=flat-square
+[npm-url]: https://npmjs.org/package/datadog-metrics
+[npm-downloads]: https://img.shields.io/npm/dm/datadog-metrics.svg?style=flat-square
+[travis-image]: https://img.shields.io/travis/dbader/node-datadog-metrics/master.svg?style=flat-square
+[travis-url]: https://travis-ci.org/dbader/node-datadog-metrics -->
--- maPong.cpp
+++ maPong.cpp
@@ -7,7 +7,6 @@
 using namespace std;
 using namespace alx;
 
-#define PI 3.14159
 
 // const float FPS = 60;
 const int SCREEN_W = 640*1.25;
@@ -21,56 +20,62 @@ class Sprite {
 public:
 
 
-    int hight;
-    int width;
+	int hight;
+	int width;
 
-    Point<int> position;
+	Point<int> position;
 
-    Point<int> velocity;
+	Point<int> velocity;
 
-    // Bitmap bitmap;
+	// Bitmap bitmap;
 
-    // ALLEGRO_COLOR sprite_color = al_map_rgb(255, 0, 0);
-    ALLEGRO_COLOR sprite_color = al_map_rgb(255, 255, 255);
+	// ALLEGRO_COLOR sprite_color = al_map_rgb(255, 0, 0);
+	ALLEGRO_COLOR sprite_color = al_map_rgb(255, 255, 255);
 
 
-    /*Sprite(const Point<int> &pos, const Bitmap &bmp) : position(pos), bitmap(bmp) {
-    }
+	/*Sprite(const Point<int> &pos, const Bitmap &bmp) 
+		: position(pos), bitmap(bmp) {
+	}
 
-    //draws the bitmap
-    void draw() const {
-        bitmap.draw(position.getX(), position.getY());
-    }
+	//draws the bitmap
+	void draw() const {
+			bitmap.draw(position.getX(), position.getY());
+	}
 */
-    
-    //get rect
-    Rect<int> getRect() const {
+	
+	//get rect
+	Rect<int> getRect() const {
 
-        Point<int> _position = - makePoint(width/2, hight/2) + position;
-        return Rect<int>(_position, makeSize(width, hight));
-    }
+		Point<int> _position = - makePoint(width/2, hight/2) + position;
+		return Rect<int>(_position, makeSize(width, hight));
+	}
 
-    Sprite(const Point<int> &pos, int _width, int _hight) : position(pos), hight(_hight), width(_width) {
-    }
+	Sprite(const Point<int> &pos, int _width, int _hight) : 
+		position(pos), hight(_hight), width(_width) {
+	}
 
-    Sprite(const Point<int> &pos) : position(pos) {
-    }
+	Sprite(const Point<int> &pos) : position(pos) {
+	}
 
 
-    void draw() {
+	void draw() {
 
-        // al_draw_rectangle(position.getX(), position.getY(), width+position.getX(), hight+position.getY(), sprite_color, 1);
-        // al_draw_filled_rectangle(position.getX(), position.getY(), width+position.getX(), hight+position.getY(), sprite_color);
+		Point<int> _position = position - makePoint(width/2, hight/2);
+		// Point<int> _position = position ;
 
-        Point<int> _position = position - makePoint(width/2, hight/2);
-        // Point<int> _position = position ;
-        al_draw_filled_rectangle(_position.getX(), _position.getY(), width+_position.getX(), hight+_position.getY(), sprite_color);
-    
-    }
+		// al_draw_rectangle(
+		al_draw_filled_rectangle(
+			_position.getX(), 
+			_position.getY(), 
+			width+_position.getX(), 
+			hight+_position.getY(), 
+			sprite_color );
 
-    // hitTest(const Sprite& sprite){ // getRect() ?? 
-    // }
-    
+	}
+
+	// hitTest(const Sprite& sprite){ // getRect() ?? 
+	// }
+	
 
 
 };
@@ -78,61 +83,66 @@ public:
 class Ball : public Sprite
 {
 
-    int color = 0;
+	int color = 0;
 public:
 
 
 
-    // Ball();
-    ~Ball(){};
+	// Ball();
+	~Ball(){};
 
-    Ball(const Point<int> &pos, int _width, int _hight) : Sprite(pos, _width, _hight) {
-    }
-
-
-    void setRandomColor(){
-
-        int i = color;
-            sprite_color = al_map_rgb((i%3==0?255:0), (i%3==1?255:0), (i%3==2?255:0));
-
-        color++;
-
-    }
+	Ball(const Point<int> &pos, int _width, int _hight) 
+		: Sprite(pos, _width, _hight) {}
 
 
+	void setRandomColor(){
 
-    void fly() {
+		int i = color;
+		sprite_color = al_map_rgb((i%3==0?255:0), (i%3==1?255:0), (i%3==2?255:0));
 
-        // setRandomColor();
+		color++;
 
-        //проверка на столкновение с краями
-        if (position.getX() >= SCREEN_W-width/2 or position.getX() <= 0+width/2)
-            velocity.setX( -velocity.getX() );
-
-        if (position.getY() >= SCREEN_H-hight/2 or position.getY() <= 0+hight/2)
-            velocity.setY( -velocity.getY() );
-
-        position += velocity;
-    }
-
-    //check collision between two sprites
-    // bool collision(const Sprite& obj) {
-    void collision(const Sprite& obj) {
-        
-        // if( getRect().intersects(obj.getRect()) ){
-        if( abs(position.getX()-obj.position.getX()) == width/2+obj.width/2  and (abs(position.getY()-obj.position.getY()) < hight/2+obj.hight/2 ) )
-        {
-            setRandomColor();
-                // velocity.setX( -abs(velocity.getX()+1.5) ); //Баги, баги, эти баги ВЕЗДЕ!
-                velocity.setX( velocity.getX() ); 
-        }
-        else if( abs(position.getY()-obj.position.getY()) == hight/2+obj.hight/2  and (abs(position.getX()-obj.position.getX()) < width/2+obj.width/2 ) )
-            velocity.setY( -velocity.getY() );
-        // else
+	}
 
 
-    }
-    
+
+	void fly() {
+
+		// setRandomColor();
+
+		//проверка на столкновение с краями
+		if (position.getX() >= SCREEN_W-width/2 or position.getX() <= 0+width/2)
+			velocity.setX( -velocity.getX() );
+
+		if (position.getY() >= SCREEN_H-hight/2 or position.getY() <= 0+hight/2)
+			velocity.setY( -velocity.getY() );
+
+		position += velocity;
+	}
+
+	//check collision between two sprites
+	// bool collision(const Sprite& obj) {
+	void collision(const Sprite& obj) {
+		
+		// if( getRect().intersects(obj.getRect()) ){
+		if( abs(position.getX()-obj.position.getX()) == width/2+obj.width/2 and
+			(abs(position.getY()-obj.position.getY()) < hight/2+obj.hight/2 ) )
+		{
+			setRandomColor();
+			//Баги, баги, эти баги ВЕЗДЕ!
+			// velocity.setX( -abs(velocity.getX()+1.5) ); 
+			velocity.setX( -velocity.getX() ); 
+		}
+		else if( 
+			abs(position.getY()-obj.position.getY()) == hight/2+obj.hight/2  and
+			(abs(position.getX()-obj.position.getX()) < width/2+obj.width/2 ) 
+			)
+			velocity.setY( -velocity.getY() );
+		// else
+
+
+	}
+		
 };
 
 
@@ -142,244 +152,259 @@ public:
 
 
 
-    // Board();
-    ~Board(){};
+	// Board();
+	~Board(){};
 
-    Board(const Point<int> &pos, int _width, int _hight) : Sprite(pos, _width, _hight) {
-    }
-
-
-
-
-    void move_down() {
-
-        //проверка на столкновение с краями
-        // if (position.getX() >= SCREEN_W or position.getX() <=0)
-            // velocity.setX( -velocity.getX() );
-
-
-        if (position.getY()+velocity.getY() <= SCREEN_H-hight/2)
-            // velocity.setY( -velocity.getY() );
-            position.setY( position.getY()+velocity.getY() ) ;
-        else
-            position.setY( SCREEN_H-hight/2 );
-    }
-
-    void move_up() {
-
-        //проверка на столкновение с краями
-        // if (position.getX() >= SCREEN_W or position.getX() <=0)
-            // velocity.setX( -velocity.getX() );
-
-        if (position.getY()-velocity.getY() >= 1+hight/2)
-            // velocity.setY( -velocity.getY() );
-            position.setY( position.getY()-velocity.getY() ) ;
-        else
-            position.setY( 1+hight/2 );
-    }
+	Board(const Point<int> &pos, int _width, int _hight) 
+		: Sprite(pos, _width, _hight) {
+	}
 
 
 
-    void autoCatch(Point<int> point){
 
-        if (position.getY() > point.getY())
-            move_up();
-        else if (position.getY() < point.getY())
-            move_down();
+	void move_down() {
 
-
-    }
+		//проверка на столкновение с краями
+		// if (position.getX() >= SCREEN_W or position.getX() <=0)
+			// velocity.setX( -velocity.getX() );
 
 
-    
+		if (position.getY()+velocity.getY() <= SCREEN_H-hight/2)
+			// velocity.setY( -velocity.getY() );
+			position.setY( position.getY()+velocity.getY() ) ;
+		else
+			position.setY( SCREEN_H-hight/2 );
+	}
+
+	void move_up() {
+
+		//проверка на столкновение с краями
+		// if (position.getX() >= SCREEN_W or position.getX() <=0)
+			// velocity.setX( -velocity.getX() );
+
+		if (position.getY()-velocity.getY() >= 1+hight/2)
+			// velocity.setY( -velocity.getY() );
+			position.setY( position.getY()-velocity.getY() ) ;
+		else
+			position.setY( 1+hight/2 );
+	}
+
+
+
+	void autoCatch(Point<int> point){
+
+		if (position.getY() > point.getY())
+			move_up();
+		else if (position.getY() < point.getY())
+			move_down();
+
+
+	}
+
+
+		
 };
 
 
 int main(int argc, char **argv){
 
-    al_init();
-    al_install_keyboard();
+	al_init();
+	al_install_keyboard();
 
 
-    Display display(SCREEN_W, SCREEN_H);
-    display.setWindowTitle("Ma Pong :-)");
+	Display display(SCREEN_W, SCREEN_H);
+	display.setWindowTitle("Ma Pong :-)");
 
-    //--------------
-    EventQueue eventQueue;
-    Timer timer(1./60);
-    UserEventSource ues;
+	//--------------
+	EventQueue eventQueue;
+	Timer timer(1./60);
+	UserEventSource ues;
 
-    //bind the resources to the event queue
-    eventQueue << Keyboard::getEventSource() /*<< Mouse::getEventSource()*/ << display << timer << ues;
-    //--------------
-
-
-    // al_clear_to_color(al_map_rgb(40,40,40)); //Цвет фона
-    Color clr1(40,40,40);
-    
+	//bind the resources to the event queue
+	eventQueue << Keyboard::getEventSource() 
+	/*<< Mouse::getEventSource()*/ << display << timer << ues;
+	//--------------
 
 
-
-
+	// al_clear_to_color(al_map_rgb(40,40,40)); //Цвет фона
+	Color clr1(40,40,40);
+	
 
 
 
 
 
-    
-
-    float hight = 25;
-    float width = 25;
-
-
-
-    Ball ball( makePoint(SCREEN_W/2, SCREEN_H/2), hight, width );
-    ball.velocity = makePoint(1, 1);
-    // ball.sprite_color = al_map_rgb(255, 0, 0);
-
-    // Board myBoard( makePoint( SCREEN_W-75-25/2 , SCREEN_H/2-150/2), 25, 150 );
-    Board myBoard( makePoint( SCREEN_W-75 , SCREEN_H/2), 25, 150 );
-    myBoard.velocity = makePoint(5, 5);
-    
-
-
-
-    Board evilBoard( makePoint( 75 , SCREEN_H/2), 25, 150 );
-    // evilBoard.velocity = makePoint(5, 5); //Багает при этом значении
-    evilBoard.velocity = makePoint(1, 1);
 
 
 
 
-    // cout << ball.position.getX() << endl;
-    // cout << ball.position.getY() << endl;
-    
+	
+
+	float hight = 25;
+	float width = 25;
+
+
+
+	Ball ball( makePoint(SCREEN_W/2, SCREEN_H/2), hight, width );
+	ball.velocity = makePoint(1, 1);
+	// ball.sprite_color = al_map_rgb(255, 0, 0);
+
+	// Board myBoard( makePoint( SCREEN_W-75-25/2 , SCREEN_H/2-150/2), 25, 150 );
+	Board myBoard( makePoint( SCREEN_W-75 , SCREEN_H/2), 25, 150 );
+	myBoard.velocity = makePoint(5, 5);
+	
+
+
+
+	Board evilBoard( makePoint( 75 , SCREEN_H/2), 25, 150 );
+	// evilBoard.velocity = makePoint(5, 5); //Багает при этом значении
+	evilBoard.velocity = makePoint(1, 1);
+
+
+
+
+	// cout << ball.position.getX() << endl;
+	// cout << ball.position.getY() << endl;
+	
 // 
 
-    // float offset = 5.5;
-    // ALLEGRO_COLOR color_orange = al_map_rgb(255, 0, 0);
-    // al_draw_rectangle(offset, offset, hight+offset, width+offset, color_orange, 1);
+	// float offset = 5.5;
+	// ALLEGRO_COLOR color_orange = al_map_rgb(255, 0, 0);
 
-    
-    ALLEGRO_EVENT_QUEUE *event_queue = NULL;
-    event_queue = al_create_event_queue();
-   
-    //event loop
-    timer.start();
-    bool loop = true;
-    bool redraw = false;
-    while(loop)
-    {
 
-        // ALLEGRO_EVENT ev;
-        // al_wait_for_event(event_queue, &ev);
+	/*al_draw_rectangle(
+		offset, 
+		offset, 
+		hight+offset, 
+		width+offset, 
+		color_orange,
+		1 );*/
+
+
+	
+	ALLEGRO_EVENT_QUEUE *event_queue = NULL;
+	event_queue = al_create_event_queue();
  
-        // if(ev.type == ALLEGRO_EVENT_DISPLAY_CLOSE) {
-            // break;
-        // }
- 
-        // if( al_is_event_queue_empty(event_queue)) {
-        if (redraw && eventQueue.isEmpty()) {
-            
-            al_clear_to_color( clr1 );
+	//event loop
+	timer.start();
+	bool loop = true;
+	bool redraw = false;
+	while(loop)
+	{
 
-            ball.draw();
+		// ALLEGRO_EVENT ev;
+		// al_wait_for_event(event_queue, &ev);
 
-            myBoard.draw();
+		// if(ev.type == ALLEGRO_EVENT_DISPLAY_CLOSE) {
+				// break;
+		// }
 
-            evilBoard.draw();
-            // evilBoard.position.setY( ball.position.getY() );
+		// if( al_is_event_queue_empty(event_queue)) {
+		if (redraw && eventQueue.isEmpty()) {
+				
+			al_clear_to_color( clr1 );
 
+			ball.draw();
 
-            al_flip_display();
+			myBoard.draw();
 
-            redraw = false;
-        }
-            ball.fly();
-            evilBoard.autoCatch( ball.position );
-
-            ball.collision( myBoard );
-            ball.collision( evilBoard );
-
-
-        //wait for event
-        Event event = eventQueue.waitForEvent();
-
-        //process event
-        switch (event.getType()) {
-            //key down
-            case ALLEGRO_EVENT_KEY_DOWN:
-                switch (event.getKeyboardKeycode()) {
-
-                    //end the loop
-                    case ALLEGRO_KEY_ESCAPE:
-                        loop = false;
-                        break;
-
-                }
-                break;
-
-            case ALLEGRO_EVENT_KEY_CHAR:
-                switch (event.getKeyboardKeycode()) {
-
-                        // ALLEGRO_KEY_LEFT, ALLEGRO_KEY_RIGHT,
-                        // ALLEGRO_KEY_UP, ALLEGRO_KEY_DOWN,
+			evilBoard.draw();
+			// evilBoard.position.setY( ball.position.getY() );
 
 
-                    case ALLEGRO_KEY_DOWN :
-                            myBoard.move_down();
-                        break;
+			al_flip_display();
 
-                    case ALLEGRO_KEY_UP :
-                            myBoard.move_up();
-                        break;
-                }
-                break;
+			redraw = false;
+		}
 
-            //mouse
-            /*case ALLEGRO_EVENT_MOUSE_AXES:
-                paddle.position.setX(std::max(0, std::min(event.getMouseX() - paddle.bitmap.getWidth()/2, display.getWidth() - paddle.bitmap.getWidth())));
-                if (ball.velocity == Point<int>(0, 0)) {
-                    ball.position.setX(paddle.position.getX() + paddle.bitmap.getWidth()/2 - ball.bitmap.getWidth()/2);
-                }
-                break;
+		ball.fly();
+		evilBoard.autoCatch( ball.position );
 
-            //button down
-            case ALLEGRO_EVENT_MOUSE_BUTTON_DOWN:
-                // if (ball.velocity == Point<int>(0, 0)) {
-                    // ball.velocity.set(2, -2);
-                    ball.sprite_color = al_map_rgb(0, 255, 0);
-                    cout << "hello" << endl;
-                // }
-                break;
-                */
-
-            //timer
-            case ALLEGRO_EVENT_TIMER:
-                if (event.getTimer() == timer) {
-                    redraw = true;
-                    // updateLogic(); //???????????????????????????????????????????????????????????????????????????
-                }
-                break;
-
-            //display close
-            case ALLEGRO_EVENT_DISPLAY_CLOSE:
-                loop = false;
-                break;
-
-        }//switch end
-   }
- 
+		ball.collision( myBoard );
+		ball.collision( evilBoard );
 
 
+		//wait for event
+		Event event = eventQueue.waitForEvent();
 
-    // al_flip_display(); //Отрисовка
-    
-    // al_rest(5.0); //Задержка в секундах
-    // al_destroy_display(display);
+		//process event
+		switch (event.getType()) {
+			//key down
+			case ALLEGRO_EVENT_KEY_DOWN:
+					switch (event.getKeyboardKeycode()) {
+
+						//end the loop
+						case ALLEGRO_KEY_ESCAPE:
+								loop = false;
+							break;
+
+					}
+				break;
+
+			case ALLEGRO_EVENT_KEY_CHAR:
+				switch (event.getKeyboardKeycode()) {
+
+							// ALLEGRO_KEY_LEFT, ALLEGRO_KEY_RIGHT,
+							// ALLEGRO_KEY_UP, ALLEGRO_KEY_DOWN,
+
+
+					case ALLEGRO_KEY_DOWN :
+							myBoard.move_down();
+						break;
+
+					case ALLEGRO_KEY_UP :
+							myBoard.move_up();
+						break;
+				}
+				break;
+
+			//mouse
+			/*case ALLEGRO_EVENT_MOUSE_AXES:
+					paddle.position.setX(std::max(0, std::min(event.getMouseX() - 
+						paddle.bitmap.getWidth()/2, display.getWidth() - 
+						paddle.bitmap.getWidth())));
+					if (ball.velocity == Point<int>(0, 0)) {
+							ball.position.setX(paddle.position.getX() + 
+								paddle.bitmap.getWidth()/2 - ball.bitmap.getWidth()/2);
+					}
+					break;
+
+			//button down
+			case ALLEGRO_EVENT_MOUSE_BUTTON_DOWN:
+					// if (ball.velocity == Point<int>(0, 0)) {
+							// ball.velocity.set(2, -2);
+							ball.sprite_color = al_map_rgb(0, 255, 0);
+							cout << "hello" << endl;
+					// }
+					break;
+					*/
+
+			//timer
+			case ALLEGRO_EVENT_TIMER:
+				if (event.getTimer() == timer) {
+					redraw = true;
+					// updateLogic(); //?????????????????????????????????????????????
+				}
+				break;
+
+			//display close
+			case ALLEGRO_EVENT_DISPLAY_CLOSE:
+				loop = false;
+				break;
+
+		}//switch end
+ }
 
 
 
 
-   return 0;
+	// al_flip_display(); //Отрисовка
+	
+	// al_rest(5.0); //Задержка в секундах
+	// al_destroy_display(display);
+
+
+
+
+	 return 0;
 }
