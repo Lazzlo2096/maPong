38d6ea5158f2c19c525a3a02a5ef34611ed2a46a
lazzlo2096 <lazzlo2096@yandex.ru>
2018-11-11 15:00:22 +0300

    Now it works\!;

--- src/main.cpp
+++ src/main.cpp
@@ -25,23 +25,20 @@ const float FPS = 60;
 
 void allegro_init()
 {
-
-}
-
-
-void qwerty()
-{
-
+	al_init();
+	al_install_keyboard();
 	
+	al_init_font_addon(); // initialize the font addon
+	al_init_ttf_addon(); // initialize the ttf (True Type Font) addon
 
+	al_init_primitives_addon(); //Исправляет assertion 'addon_initialized' failed
 }
-
 
 class MyGame
 {
 
 	alx::Timer timer;
-	alx::EventQueue eventQueue; // нужно сначало init allegro
+	alx::EventQueue eventQueue; // нужно сначало сделать init allegro
 
 	mainScene myMainScene;
 
@@ -49,338 +46,170 @@ public:
 	MyGame() :
 		timer(1./FPS)
 		//,eventQueue()
-	{};
+	{}
 	
 	~MyGame(){};
-	
 
-
-
-void init()
-{
-
-
-	//Мб начиная от сюдова всё запихнуть в объект сцены
-	alx::Display display(SCREEN_W, SCREEN_H);
-	display.setWindowTitle("Ma Pong :-)");
-
-	//--------------
-	
-	//alx::Timer timer(1./FPS);
-	alx::UserEventSource ues;
-
-	//bind the resources to the event queue
-	eventQueue << alx::Keyboard::getEventSource() 
-	/*<< Mouse::getEventSource()*/ << display << timer << ues;
-	//--------------
-
-	
-	myMainScene.init();
-
-	/*
-	cout << ball.position.getX() << endl;
-	cout << ball.position.getY() << endl;
-	*/
-
-	// float offset = 5.5;
-	// ALLEGRO_COLOR color_orange = al_map_rgb(255, 0, 0);
-
-	/*al_draw_rectangle(
-		offset, 
-		offset, 
-		height+offset, 
-		width+offset, 
-		color_orange,
-		1 );*/
-
-	//ALLEGRO_EVENT_QUEUE *event_queue = NULL;
-	//event_queue = al_create_event_queue();
- 
- 	//Запихнуть в метод run() сцены
-	//event loop // event обработчик
-
-}
-
-void run()
-{
-	
-	bool loop = true;
-	bool is_redraw = false;
-	timer.start();
-	while (loop)
+	void init()
 	{
+		//Мб начиная от сюдова всё запихнуть в объект сцены
+		alx::Display display(SCREEN_W, SCREEN_H);
+		display.setWindowTitle("Ma Pong :-)");
 
-		// ALLEGRO_EVENT ev;
-		// al_wait_for_event(event_queue, &ev);
+		//--------------
+		
+		//alx::Timer timer(1./FPS);
+		alx::UserEventSource ues;
 
-		// if(ev.type == ALLEGRO_EVENT_DISPLAY_CLOSE) {
-				// break;
-		// }
+		//bind the resources to the event queue
+		eventQueue << alx::Keyboard::getEventSource() 
+		/*<< Mouse::getEventSource()*/ << display << timer << ues;
+		//--------------
 
-		// if( al_is_event_queue_empty(event_queue)) {
-		if (is_redraw and eventQueue.isEmpty()) {
+		
+		myMainScene.init();
 
-			myMainScene.calcAndDrawFrame();
+		/*
+		cout << ball.position.getX() << endl;
+		cout << ball.position.getY() << endl;
+		*/
 
-			al_flip_display();
+		// float offset = 5.5;
+		// ALLEGRO_COLOR color_orange = al_map_rgb(255, 0, 0);
 
-			is_redraw = false;
-		}
+		/*al_draw_rectangle(
+			offset, 
+			offset, 
+			height+offset, 
+			width+offset, 
+			color_orange,
+			1 );*/
 
-		//wait for event
-		alx::Event event = eventQueue.waitForEvent();
+		//ALLEGRO_EVENT_QUEUE *event_queue = NULL;
+		//event_queue = al_create_event_queue();
 
-		//process event
-		switch (event.getType()) {
-			
-			case ALLEGRO_EVENT_KEY_DOWN: //key down pressed
+		//======================================================
+	 
+	 	//Запихнуть в метод run() сцены
+		//event loop // event обработчик
+		
+		bool loop = true;
+		bool is_redraw = false;
+		timer.start();
+		while (loop)
+		{
+
+			// ALLEGRO_EVENT ev;
+			// al_wait_for_event(event_queue, &ev);
+
+			// if(ev.type == ALLEGRO_EVENT_DISPLAY_CLOSE) {
+					// break;
+			// }
+
+			// if( al_is_event_queue_empty(event_queue)) {
+			if (is_redraw and eventQueue.isEmpty()) {
+
+				myMainScene.calcAndDrawFrame(); //БАГ с clear_to() ! - это должно быть в одном методе с верхушкой
+
+				al_flip_display();
+
+				is_redraw = false;
+			}
+
+			//wait for event
+			alx::Event event = eventQueue.waitForEvent();
+
+			//process event
+			switch (event.getType()) {
+				
+				case ALLEGRO_EVENT_KEY_DOWN: //key down pressed
+						switch (event.getKeyboardKeycode()) {
+
+							//end the loop
+							case ALLEGRO_KEY_ESCAPE:
+									loop = false;
+								break;
+						}
+					break;
+
+				case ALLEGRO_EVENT_KEY_CHAR:
 					switch (event.getKeyboardKeycode()) {
 
-						//end the loop
-						case ALLEGRO_KEY_ESCAPE:
-								loop = false;
+								// ALLEGRO_KEY_LEFT, ALLEGRO_KEY_RIGHT,
+								// ALLEGRO_KEY_UP, ALLEGRO_KEY_DOWN,
+
+
+						case ALLEGRO_KEY_DOWN :
+								myMainScene.onKeyDown();
+							break;
+
+						case ALLEGRO_KEY_UP :
+								myMainScene.onKeyUp();
 							break;
 					}
-				break;
+					break;
 
-			case ALLEGRO_EVENT_KEY_CHAR:
-				switch (event.getKeyboardKeycode()) {
-
-							// ALLEGRO_KEY_LEFT, ALLEGRO_KEY_RIGHT,
-							// ALLEGRO_KEY_UP, ALLEGRO_KEY_DOWN,
-
-
-					case ALLEGRO_KEY_DOWN :
-							myMainScene.onKeyDown();
+				//mouse
+				/*case ALLEGRO_EVENT_MOUSE_AXES:
+						paddle.position.setX(std::max(0, 
+							std::min(event.getMouseX() - 
+							paddle.bitmap.getWidth()/2, display.getWidth() - 
+							paddle.bitmap.getWidth())));
+						if (ball.velocity == Point<int>(0, 0)) {
+								ball.position.setX(paddle.position.getX() + 
+									paddle.bitmap.getWidth()/2 - 
+									ball.bitmap.getWidth()/2);
+						}
 						break;
 
-					case ALLEGRO_KEY_UP :
-							myMainScene.onKeyUp();
+				//button down
+				case ALLEGRO_EVENT_MOUSE_BUTTON_DOWN:
+						// if (ball.velocity == Point<int>(0, 0)) {
+								// ball.velocity.set(2, -2);
+								ball.sprite_color = al_map_rgb(0, 255, 0);
+								cout << "hello" << endl;
+						// }
 						break;
-				}
-				break;
+						*/
 
-			//mouse
-			/*case ALLEGRO_EVENT_MOUSE_AXES:
-					paddle.position.setX(std::max(0, 
-						std::min(event.getMouseX() - 
-						paddle.bitmap.getWidth()/2, display.getWidth() - 
-						paddle.bitmap.getWidth())));
-					if (ball.velocity == Point<int>(0, 0)) {
-							ball.position.setX(paddle.position.getX() + 
-								paddle.bitmap.getWidth()/2 - 
-								ball.bitmap.getWidth()/2);
+				//timer
+				case ALLEGRO_EVENT_TIMER:
+					if (event.getTimer() == timer) {
+						is_redraw = true;
+						// updateLogic(); //???????????????????????????????????
 					}
 					break;
 
-			//button down
-			case ALLEGRO_EVENT_MOUSE_BUTTON_DOWN:
-					// if (ball.velocity == Point<int>(0, 0)) {
-							// ball.velocity.set(2, -2);
-							ball.sprite_color = al_map_rgb(0, 255, 0);
-							cout << "hello" << endl;
-					// }
+				//display close
+				case ALLEGRO_EVENT_DISPLAY_CLOSE:
+					loop = false;
 					break;
-					*/
 
-			//timer
-			case ALLEGRO_EVENT_TIMER:
-				if (event.getTimer() == timer) {
-					is_redraw = true;
-					// updateLogic(); //???????????????????????????????????
-				}
-				break;
-
-			//display close
-			case ALLEGRO_EVENT_DISPLAY_CLOSE:
-				loop = false;
-				break;
-
-		}// switch end
-	}// while (loop)
+			}// switch end
+		}// while (loop)
 
 
-	// al_flip_display(); //Отрисовка
-	
-	// al_rest(5.0); //Задержка в секундах
-	// al_destroy_display(display);
+		// al_flip_display(); //Отрисовка
+		
+		// al_rest(5.0); //Задержка в секундах
+		// al_destroy_display(display);
 
-}
+	}
 
 };
 
 
 int main(int argc, char **argv)
 {
-
-al_init();
-	al_install_keyboard();
 	
-	al_init_font_addon(); // initialize the font addon
-	al_init_ttf_addon(); // initialize the ttf (True Type Font) addon
-
-	al_init_primitives_addon(); //Исправляет assertion 'addon_initialized' failed
-
-	//Мб начиная от сюдова всё запихнуть в объект сцены
-	alx::Display display(SCREEN_W, SCREEN_H);
-	display.setWindowTitle("Ma Pong :-)");
-
-	//--------------
-
-	alx::Timer timer;
-	alx::EventQueue eventQueue; // нужно сначало init allegro
-	
-	//alx::Timer timer(1./FPS);
-	alx::UserEventSource ues;
-
-	//bind the resources to the event queue
-	eventQueue << alx::Keyboard::getEventSource() 
-	/*<< Mouse::getEventSource()*/ << display << timer << ues;
-	//--------------
-
-
-	mainScene myMainScene;
-
-	
-	myMainScene.init();
-
-
-
-	/*
-	cout << ball.position.getX() << endl;
-	cout << ball.position.getY() << endl;
-	*/
-
-	// float offset = 5.5;
-	// ALLEGRO_COLOR color_orange = al_map_rgb(255, 0, 0);
-
-	/*al_draw_rectangle(
-		offset, 
-		offset, 
-		height+offset, 
-		width+offset, 
-		color_orange,
-		1 );*/
-
-	//ALLEGRO_EVENT_QUEUE *event_queue = NULL;
-	//event_queue = al_create_event_queue();
- 
- 	//Запихнуть в метод run() сцены
-	//event loop // event обработчик
-
-	
-	bool loop = true;
-	bool is_redraw = false;
-	timer.start();
-	while (loop)
-	{
-
-		// ALLEGRO_EVENT ev;
-		// al_wait_for_event(event_queue, &ev);
-
-		// if(ev.type == ALLEGRO_EVENT_DISPLAY_CLOSE) {
-				// break;
-		// }
-
-		// if( al_is_event_queue_empty(event_queue)) {
-		if (is_redraw and eventQueue.isEmpty()) {
-
-			myMainScene.calcAndDrawFrame();
-
-			al_flip_display();
-
-			is_redraw = false;
-		}
-
-		//wait for event
-		alx::Event event = eventQueue.waitForEvent();
-
-		//process event
-		switch (event.getType()) {
-			
-			case ALLEGRO_EVENT_KEY_DOWN: //key down pressed
-					switch (event.getKeyboardKeycode()) {
-
-						//end the loop
-						case ALLEGRO_KEY_ESCAPE:
-								loop = false;
-							break;
-					}
-				break;
-
-			case ALLEGRO_EVENT_KEY_CHAR:
-				switch (event.getKeyboardKeycode()) {
-
-							// ALLEGRO_KEY_LEFT, ALLEGRO_KEY_RIGHT,
-							// ALLEGRO_KEY_UP, ALLEGRO_KEY_DOWN,
-
-
-					case ALLEGRO_KEY_DOWN :
-							myMainScene.onKeyDown();
-						break;
-
-					case ALLEGRO_KEY_UP :
-							myMainScene.onKeyUp();
-						break;
-				}
-				break;
-
-			//mouse
-			/*case ALLEGRO_EVENT_MOUSE_AXES:
-					paddle.position.setX(std::max(0, 
-						std::min(event.getMouseX() - 
-						paddle.bitmap.getWidth()/2, display.getWidth() - 
-						paddle.bitmap.getWidth())));
-					if (ball.velocity == Point<int>(0, 0)) {
-							ball.position.setX(paddle.position.getX() + 
-								paddle.bitmap.getWidth()/2 - 
-								ball.bitmap.getWidth()/2);
-					}
-					break;
-
-			//button down
-			case ALLEGRO_EVENT_MOUSE_BUTTON_DOWN:
-					// if (ball.velocity == Point<int>(0, 0)) {
-							// ball.velocity.set(2, -2);
-							ball.sprite_color = al_map_rgb(0, 255, 0);
-							cout << "hello" << endl;
-					// }
-					break;
-					*/
-
-			//timer
-			case ALLEGRO_EVENT_TIMER:
-				if (event.getTimer() == timer) {
-					is_redraw = true;
-					// updateLogic(); //???????????????????????????????????
-				}
-				break;
-
-			//display close
-			case ALLEGRO_EVENT_DISPLAY_CLOSE:
-				loop = false;
-				break;
-
-		}// switch end
-	}// while (loop)
-
-
-	// al_flip_display(); //Отрисовка
-	
-	// al_rest(5.0); //Задержка в секундах
-	// al_destroy_display(display);
-	//==============
-
 
 	allegro_init();
 
-	qwerty();
-
-	/*MyGame maPong;
+	MyGame maPong;
 
 	maPong.init();
 
-	maPong.run();*/
+	//maPong.run();
 
 	return 0;
 }
